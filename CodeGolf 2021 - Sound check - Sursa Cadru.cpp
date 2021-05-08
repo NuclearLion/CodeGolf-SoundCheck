96c9298988e206305028fa6988538438fb958b03
Dominic <dando.ds11@gmail.com>
2021-05-08 14:31:48 +0300

    Test 11 working

--- CodeGolf 2021 - Sound check - Sursa Cadru.cpp
+++ CodeGolf 2021 - Sound check - Sursa Cadru.cpp
@@ -36,50 +36,107 @@ ofstream fout("output.out");
 
 void f(unsigned char n, unsigned char m, unsigned char ns, s *ss) {
     static vector<vector<string>> heatmap;
+
+    auto displayMatrix = [&]() {//good
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j)
+                cout << heatmap[i][j] << " ";
+            cout << '\n';
+        }
+    };
+
+    auto stringCreator = [&](int value) {
+        string space;
+        if (value > 99)
+            space = to_string(value);
+        else if (value > 9)
+            space = "0" + to_string(value);
+        else
+            space = "00" + to_string(value);
+        if(value == 0)
+            space = "===";
+        return space;
+    };//good
+
     heatmap.resize(n, vector<string>(m, "==="));
 
-    if(ns != 0)
-    {
-        s *it = ss;
-        //while(it != NULL)
-        //{
-            switch (it->i) {
-                case 1:
-                    heatmap[it->l - 1][it->c - 1] = "00" + to_string(it->i);
-                    break;
-                case 3:
-                    heatmap[it->l - 1][it->c - 1] = "00" + to_string(it->i);
-                    int itr = it->i;
-                    for(int i = it->l; i <= it->l + 1; ++i)
-                    {
-                        --itr;
-                        heatmap[i][it->c - 1] = "00" + to_string(itr);
-                    }
-                    itr = it->i;
-                    for(int j = it->c; j <= it->c + 1; ++j)
-                    {
-                        --itr;
-                        heatmap[it->l - 1][j] = "00" + to_string(itr);
-                    }
-                    int i = it->l;
-                    int j = it->c;
-                    itr = it->i;
-                    while (i <= it->l + 1)
-                    {
-                        --itr;
-                        heatmap[i][j] = "00" + to_string(itr);
-                        ++i, ++j;
-                    }
+    if (ns != 0) {
+        for (int it = 0; it < ns; ++it) {
+            int len = ss[it].i;
+            int intensity = ss[it].i;
+            heatmap[ss[it].l - 1][ss[it].c - 1] = stringCreator(intensity);
+            intensity--;
+            ///line -
+            for (int j = ss[it].c; j <= ss[it].c + len - 2; ++j)
+                if (j < m) {
+                    heatmap[ss[it].l - 1][j] = stringCreator(intensity);
+                    intensity--;
+                } //todo this 7 more times (total 8) maybe do func
+            intensity = ss[it].i - 1;
+            for (int j = ss[it].c - 2; j >= ss[it].c - len; --j)
+                if (j >= 0) {
+                    heatmap[ss[it].l - 1][j] = stringCreator(intensity);
+                    intensity--;
+                }
+
+            ///column |
+            intensity = ss[it].i - 1;
+            for (int i = ss[it].l; i <= ss[it].l + len - 2; ++i)
+                if (i < n) {
+                    heatmap[i][ss[it].c - 1] = stringCreator(intensity);
+                    intensity--;
+                }
+            intensity = ss[it].i - 1;
+            for (int i = ss[it].l - 2; i >= ss[it].l - len; --i)
+                if (i >= 0) {
+                    heatmap[i][ss[it].c - 1] = stringCreator(intensity);
+                    intensity--;
+                }
+
+            ///diagonal /
+            int i = ss[it].l - 2;
+            int j = ss[it].c;
+            intensity = ss[it].i - 1;
+            while (j < ss[it].c + len - 1 && j < m && i >= 0) {
+                heatmap[i][j] = stringCreator(intensity);
+                intensity--;
+                j++;
+                i--;
             }
-        //}
+            i = ss[it].l;
+            j = ss[it].c - 2;
+            intensity = ss[it].i - 1;
+            while (i < ss[it].l + len - 1 && j >= 0 && i < n) {
+                heatmap[i][j] = stringCreator(intensity);
+                intensity--;
+                j--;
+                i++;
+
+            }
+
+            ///diagonal '\'
+            i = ss[it].l;
+            j = ss[it].c;
+            intensity = ss[it].i - 1;
+            while (i < ss[it].l + len - 1 && j < m && i < n) {
+                heatmap[i][j] = stringCreator(intensity);
+                intensity--;
+                j++;
+                i++;
+            }
+            i = ss[it].l - 2;
+            j = ss[it].c - 2;
+            intensity = ss[it].i - 1;
+            while (i >= ss[it].l - len  && j >= 0 && i >= 0) {
+                heatmap[i][j] = stringCreator(intensity);
+                intensity--;
+                j--;
+                i--;
+            }
+        }
     }
 
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = 0; j < m; ++j)
-            cout << heatmap[i][j] << " ";
-        cout << '\n';
-    }
+    displayMatrix();
 }
 
 int main(int argc, char **argv) {
